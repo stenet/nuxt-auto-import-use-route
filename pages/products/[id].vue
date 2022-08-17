e51cd12b5a25791fcf9d74e9c7adc03477e0f27e
Stefan Heim <stefan.heim@tip.co.at>
2022-08-17 13:52:08 +0200

    added current route info

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -2,12 +2,18 @@
 //import { useRoute } from "vue-router";
 
 const route = useRoute();
+const path = route.path;
 const id = route.params.id || "undefined";
 </script>
 
 <template>
   <div>
-    Product {{ id }}
+    <div>
+      Product {{ id }}
+    </div>
+    <div>
+      Current route <b>{{ path }}</b> - should be <b>/product/123</b>
+    </div>
   </div>
 </template>
 
